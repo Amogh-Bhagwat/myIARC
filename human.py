b828f45f9232765232217b0f411fd403caf571b1
Kaushal Kishore <kaushore@gmail.com>
2018-03-29 19:49:15 +0530

    Add files via upload

--- human.py
+++ human.py
@@ -1,101 +1,75 @@
-from __future__ import print_function
-
-import roslib
-roslib.load_manifest('my_package')
-
-
+import rospy
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+import cv2
 from imutils.video import VideoStream
 from imutils.video import FPS
 import numpy as np
 import argparse
 import imutils
 import time
-import cv2
-import sys
-import rospy
-from std_msgs.msg import String
-from sensor_msgs.msg import Image
-from cv_bridge import CvBridge, CvBridgeError
+
 
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-p", "--prototxt", required=True,help="path to Caffe 'deploy' prototxt file")
-ap.add_argument("-m", "--model", required=True,	help="path to Caffe pre-trained model")
+ap.add_argument("-m", "--model", required=True, help="path to Caffe pre-trained model")
 ap.add_argument("-c", "--confidence", type=float, default=0.2,help="minimum probability to filter weak detections")
 args = vars(ap.parse_args())
 
 CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat","bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
-	"dog", "horse", "motorbike", "person", "pottedplant", "sheep","sofa", "train", "tvmonitor"]
+    "dog", "horse", "motorbike", "person", "pottedplant", "sheep","sofa", "train", "tvmonitor"]
 COLORS = np.random.uniform(0, 0, size=(len(CLASSES), 3))
 
 print("[INFO] loading model...")
 net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
- 
-vs = VideoStream(src=1).start()
-time.sleep(2.0)
-fps = FPS().start()
 
-class image_converter:
 
-  def __init__(self):
-    self.image_pub = rospy.Publisher("image_topic_2",Image)
+# Instantiate CvBridge
+bridge = CvBridge()
 
-    self.bridge = CvBridge()
-    self.image_sub = rospy.Subscriber("image_topic",Image,self.callback)
-
-  def callback(self,data):
+def image_callback(msg):
+    print("Received an image!")
     try:
-      cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
-    except CvBridgeError as e:
-      print(e)
+        # Convert your ROS Image message to OpenCV2
+        frame = bridge.imgmsg_to_cv2(msg, "bgr8")
+    except CvBridgeError, e:
+        print(e)
+    else:
+        frame=frame.read()
+        frame = imutils.resize(frame, width=400)
 
-	
-while True:
-	frame = vs.read()
-	frame = imutils.resize(frame, width=400)
+        (h, w) = frame.shape[:2]
+        blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)),0.007843, (300, 300), 127.5)
 
-	(h, w) = frame.shape[:2]
-	blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)),0.007843, (300, 300), 127.5)
+        net.setInput(blob)
+        detections = net.forward()
+        for i in np.arange(0, detections.shape[2]):
+            confidence = detections[0, 0, i, 2]
+            if confidence > args["confidence"]:
+                idx = int(detections[0, 0, i, 1])
 
-	net.setInput(blob)
-	detections = net.forward()
-	for i in np.arange(0, detections.shape[2]):
-		confidence = detections[0, 0, i, 2]
-		if confidence > args["confidence"]:
-			idx = int(detections[0, 0, i, 1])
+                if idx==15:
+                    box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+                    (startX, startY, endX, endY) = box.astype("int")
+                    print("("+str(startX)+","+str(startY)+")  ("+str(endX)+" "+str(endY)+")"+"   centeroid : ("+str((startX+endX)/2)+"),("+str((startY+endY)/2)+")" )
+                    label = "{}: {:.2f}%".format(CLASSES[idx],confidence * 100)
+                    cv2.rectangle(frame, (startX, startY), (endX, endY),
+                        COLORS[idx], 2)
+                    y = startY - 15 if startY - 15 > 15 else startY + 15
+                    cv2.putText(frame, label, (startX, y),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
+        cv2.imshow("Frame", frame)
+        #cv2.imwrite('camera_image.jpeg', cv2_img)
 
-			if idx==15:
-				box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-				(startX, startY, endX, endY) = box.astype("int")
-				print("("+str(startX)+","+str(startY)+")  ("+str(endX)+" "+str(endY)+")"+"   centeroid : ("+str((startX+endX)/2)+"),("+str((startY+endY)/2)+")" )
-				label = "{}: {:.2f}%".format(CLASSES[idx],confidence * 100)
-				cv2.rectangle(frame, (startX, startY), (endX, endY),
-					COLORS[idx], 2)
-				y = startY - 15 if startY - 15 > 15 else startY + 15
-				cv2.putText(frame, label, (startX, y),
-					cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
-
-	cv2.imshow("Frame", frame)
-	key = cv2.waitKey(1) & 0xFF
-	if key == ord("q"):
-		break
-	fps.update()
-fps.stop()
-print("[INFO] elapsed time: {:.2f}".format(fps.elapsed()))
-print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
-
-
-def main(args):
-  ic = image_converter()
-  rospy.init_node('image_converter', anonymous=True)
-  try:
+def main():
+    rospy.init_node('image_listener')
+    # Define your image topic
+    image_topic = "/camera2/image_raw"
+    # Set up your subscriber and define its callback
+    rospy.Subscriber(image_topic, Image, image_callback)
+    # Spin until ctrl + c
     rospy.spin()
-  except KeyboardInterrupt:
-    print("Shutting down")
-  cv2.destroyAllWindows()
 
 if __name__ == '__main__':
-    main(sys.argv)
-
-cv2.destroyAllWindows()
-vs.stop()
+    main()
