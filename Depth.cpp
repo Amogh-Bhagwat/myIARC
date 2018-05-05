f8eaf5b6c72d93a5bb141a05e9a9e397cd25ff7a
Kaushal Kishore <kaushore@gmail.com>
2018-05-05 13:21:34 +0530

    Add files via upload

--- Depth.cpp
+++ Depth.cpp
@@ -5,21 +5,22 @@
 #include <pcl/point_types.h>
 #include "std_msgs/Float64MultiArray.h"
 #include "geometry_msgs/Point.h"
-// #include <message_filters/subscriber.h>
-// #include <message_filters/synchronizer.h>
+#include <message_filters/subscriber.h>
+#include <message_filters/synchronizer.h>
+#include <message_filters/time_synchronizer.h>
+#include <message_filters/sync_policies/exact_time.h>
 
-// #include <message_filters/time_synchronizer.h>
-// #include <message_filters/sync_policies/exact_time.h>
 
 using namespace sensor_msgs;
 using namespace message_filters;
 using namespace std_msgs;
 
+
 geometry_msgs::Point humanDepth;
+
 ros::Publisher pub;
 
-
-void callback(const sensor_msgs::PointCloud2ConstPtr& input, const std_msgs::Float64MultiArrayConstPtr& Coordinate)
+void callback(const sensor_msgs::PointCloud2ConstPtr& input, const std_msgs::Float64MultiArrayConstPtr& Coordinate,ros::NodeHandle *nh, ros::Publisher *pub)
 {
 
 pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZRGB>);
@@ -38,10 +39,12 @@ for(int i=Coordinate->data[0]; i<Coordinate->data[2] ; i++){
 	
 }
 
+*pub = nh->advertise<geometry_msgs::Point>("/humanDepth", 1);
+
 humanDepth.x= 0;
 humanDepth.y=0;
 humanDepth.z=sum/count;
-pub.publish(humanDepth);
+pub->publish(humanDepth);
 
 }
 
@@ -51,6 +54,7 @@ int main (int argc, char** argv)
 {
   	// Initialize ROS
   	ros::init (argc, argv, "human distance");
+  	
   	ros::NodeHandle nh;
 
   	message_filters::Subscriber<PointCloud2> depth_sub(nh, "/camera/depth/points", 1);
@@ -59,11 +63,11 @@ int main (int argc, char** argv)
   	// TimeSynchronizer<PointCloud2, Float64MultiArray> sync(depth_sub, point_sub, 10);
   	// sync.registerCallback(boost::bind(&callback, _1, _2));
 
-  	//typedef sync_policies::ExactTime<PointCloud2, Float64MultiArray> MySyncPolicy;
+  	typedef sync_policies::ExactTime<PointCloud2, Float64MultiArray> MySyncPolicy;
 
 
-  	//Synchronizer<MySyncPolicy> sync(MySyncPolicy(10), depth_sub, point_sub);
-  	//sync.registerCallback(boost::bind(&callback, _1, _2));
+  	Synchronizer<MySyncPolicy> sync(MySyncPolicy(10), depth_sub, point_sub);
+  	sync.registerCallback(boost::bind(&callback, _1, _2, &nh, &pub));
 
   	// Create a ROS subscriber for the input point cloud
   	//ros::Subscriber sub1 = nh.subscribe ("/camera/stereo_camera_LR/points2", 1, cloud_cb);
@@ -72,7 +76,7 @@ int main (int argc, char** argv)
 
   	// Create a ROS publisher for the output model coefficients
   	//pub = nh.advertise<pcl_msgs::ModelCoefficients> ("output", 1);
-  	pub = nh.advertise<geometry_msgs::Point>("/humanDepth", 1);
+  	
 	//pub = nh.advertise<sensor_msgs::PointCloud2> ("output", 1);
   	// Spin
   	ros::spin ();
