5e65e944ca59cda9e5c71f694e42feb15a6f10e0
Kaushal Kishore <kaushore@gmail.com>
2018-05-08 20:49:49 +0530

    Add files via upload

--- Depth.cpp
+++ Depth.cpp
@@ -6,8 +6,6 @@
 #include "std_msgs/Float64MultiArray.h"
 #include "geometry_msgs/Point.h"
 #include <message_filters/subscriber.h>
-#include <message_filters/synchronizer.h>
-#include <message_filters/time_synchronizer.h>
 #include <message_filters/sync_policies/exact_time.h>
 #include <ros/time.h>
 #include <pcl/console/parse.h>
@@ -39,20 +37,20 @@ void callback0(const std_msgs::Float64MultiArrayConstPtr& coordinate)
 void callback1(const sensor_msgs::PointCloud2ConstPtr& input)
 {
 
-pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZRGB>);
+pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
 pcl::fromROSMsg(*input, *cloud);
 //std::cout<<cloud->at(500,500)<<std::endl;
 float sum=0;
 int count=0;
 
 for(int i=Coordinate[0]; i<Coordinate[2] ; i++){
-	for(int j=Coordinate[1] ; j<Coordinate[3] ; j++){
-		if(cloud->at(i,j).z>0){
-		sum+= cloud->at(i,j).z;
-		count++;	
-		}
-	}
-	
+    for(int j=Coordinate[1] ; j<Coordinate[3] ; j++){
+        if(cloud->at(i,j).z>0){
+        sum+= cloud->at(i,j).z;
+        count++;    
+        }
+    }
+    
 }
 
 humanDepth.x= 0;
@@ -66,21 +64,21 @@ pub.publish(humanDepth);
 
 int main (int argc, char** argv)
 {
-  	
-  	ros::init (argc, argv, "human distance");
-  	
-  	ros::NodeHandle nh;
+    
+    ros::init (argc, argv, "human distance");
+    
+    ros::NodeHandle nh;
     ros::Subscriber sub1 = nh.subscribe ("/humanTracking/point", 1, callback0);
     ros::Subscriber sub2 = nh.subscribe ("/camera/depth/points", 1, callback1);
 
     pub = nh.advertise<geometry_msgs::Point>("/humanDepth", 1);
 
-  	ros::Rate loop_rate(12); //adjust the loop rate !!! according to the network
+    ros::Rate loop_rate(12); //adjust the loop rate,according to the network
 
     while(ros::ok())
     { 
       ros::spinOnce();
       loop_rate.sleep();
     }
-  	return 0;
+    return 0;
 }
